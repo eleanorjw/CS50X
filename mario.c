f8071db079363e4f95e071548048440ca6df163b
eleanorjw <eleanorjw@users.noreply.github.com>
2020-11-23 07:53:49 +0000

    automated commit by check50 [check50=True]

--- mario.c
+++ mario.c
@@ -4,18 +4,19 @@
 int main(void)
 {
     int n;
+    //prompt for int between 1 & 8 inclusive
     do
     {
         n = get_int("Height:");
     }
-    while( n < 1 || n > 8 );
-    for (int i = 0; i < n; i++ )
+    while (n < 1 || n > 8);
+    for (int i = 0; i < n; i++)
     {
-        for (int j = 1+i; j < n; j++)
+        for (int j = 1 + i; j < n; j++)
         {
             printf(" ");
         }
-         for (int k = n-1-i; k < n; k++)
+        for (int k = 0; k < 1 + i; k++)
         {
             printf("#");
         }
