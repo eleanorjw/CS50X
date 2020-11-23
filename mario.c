bfb5aba136b6d502195be5b120f9908f3ebda3b1
eleanorjw <eleanorjw@users.noreply.github.com>
2020-11-23 08:00:28 +0000

    automated commit by submit50

--- mario.c
+++ mario.c
@@ -4,18 +4,21 @@
 int main(void)
 {
     int n;
-    //prompt for int between 1 & 8 inclusive
+    //prompt for n between 1 & 8 inclusive
     do
     {
         n = get_int("Height:");
     }
     while (n < 1 || n > 8);
+    //print n*n block
     for (int i = 0; i < n; i++)
     {
+        //print decreasing space for each line
         for (int j = 1 + i; j < n; j++)
         {
             printf(" ");
         }
+        //print increasing # for each line
         for (int k = 0; k < 1 + i; k++)
         {
             printf("#");
