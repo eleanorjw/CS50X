02206662baf88765e24475ac8ef4b58a7a508cef
eleanorjw <eleanorjw@users.noreply.github.com>
2020-11-25 18:38:34 +0000

    automated commit by submit50

--- readability.c
+++ readability.c
@@ -11,25 +11,25 @@ int main(void)
     int S = 0;
     string text = get_string("Text: ");
     //find numbers of letters
-    for(int i = 0, n = strlen(text); i < n; i++)
+    for (int i = 0, n = strlen(text); i < n; i++)
     {
-        if( isalpha( text[i]))
+        if (isalpha(text[i]))
         {
             L++;
         }
     }
     //find numbers of words
-    for(int i = 0, n = strlen(text); i < n; i++)
+    for (int i = 0, n = strlen(text); i < n; i++)
     {
-        if( text[i] == ' ')
+        if (text[i] == ' ')
         {
             W++;
         }
     }
     //find numbers of sentences
-    for(int i = 0, n = strlen(text); i < n; i++)
+    for (int i = 0, n = strlen(text); i < n; i++)
     {
-        if( text[i] == '.' || text[i] == '?' || text[i] == '!')
+        if (text[i] == '.' || text[i] == '?' || text[i] == '!')
         {
             S++;
         }
@@ -39,11 +39,11 @@ int main(void)
     //round to int
     int grade = round(index);
     //print outcome
-    if(grade < 1)
+    if (grade < 1)
     {
         printf("Before Grade 1\n");
     }
-    else if(grade >= 16)
+    else if (grade >= 16)
     {
         printf("Grade 16+\n");
     }
