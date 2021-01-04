95b44bf204634a3da2cf79168aac6b42dc9dae61
eleanorjw <eleanorjw@users.noreply.github.com>
2021-01-04 06:52:16 +0000

    automated commit by check50 [check50=True]

--- volume.c
+++ volume.c
@@ -40,10 +40,10 @@ int main(int argc, char *argv[])
 
     // TODO: Read samples from input file and write updated data to output file
     int16_t buffer;
-    while(fread(&buffer, sizeof(buffer), 1, input))
+    while(fread(&buffer, 2, 1, input))
     {
-        buffer = buffer * factor;
-        fwrite(&buffer, sizeof(buffer), 1, output);
+        buffer *= factor;
+        fwrite(&buffer, 2, 1, output);
     }
 
     // Close files
