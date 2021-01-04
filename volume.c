c9acb4b1ba83513b7ed94b64c6ba22d9d8d25df0
eleanorjw <eleanorjw@users.noreply.github.com>
2021-01-04 05:49:08 +0000

    automated commit by check50 [check50=True]

--- volume.c
+++ volume.c
@@ -35,8 +35,8 @@ int main(int argc, char *argv[])
 
     // TODO: Copy header from input file to output file
     uint8_t header[44];
-    fread(&header[44], sizeof(header[44]), 44, input);
-    fwrite(&header[44], sizeof(header[44]), 44, output);
+    fread(&header, sizeof(header), 44, input);
+    fwrite(&header, sizeof(header), 44, output);
 
     // TODO: Read samples from input file and write updated data to output file
     int16_t buffer;
