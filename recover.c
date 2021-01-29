32239431bd7a079f2089c362db0e0d9b122eed85
eleanorjw <eleanorjw@users.noreply.github.com>
2021-01-29 06:36:07 +0000

    automated commit by check50 [check50=True]

--- recover.c
+++ recover.c
@@ -31,33 +31,31 @@ int main(int argc, char *argv[])
  int found = 0;
  int fileopen = 0;
  //Read file to find jpg n create jpg
- while (fread(buffer, BLOCK * BYTE, 1, inptr))
+ while (fread(buffer, BLOCK, 1, inptr))
  {
      //find jpg
      if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xff) == 0xe0 )
      {
         if (found == 0)
         {
-            sprintf(imgn,"000.jpg");
-            img = fopen(imgn, "w");
             fileopen = 1;
         }
         else
         {
             fclose(img);
-            sprintf(imgn,"%03i.jpg", found++);
-            img = fopen(imgn, "w");
         }
+        sprintf(imgn,"%03i.jpg", found++);
+        img = fopen(imgn, "w");
         if (img == NULL)
         {
             return 1;
         }
-        fwrite(buffer, BLOCK * BYTE, 1, img);
+        fwrite(buffer, BLOCK, 1, img);
      }
      //write blocks of jpg
      if (fileopen == 1)
      {
-         fwrite(buffer, BLOCK * BYTE, 1, img);
+         fwrite(buffer, BLOCK, 1, img);
      }
  }
  fclose(img);
