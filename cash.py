33dae9a9ebef1f2f228354bf440dd656770c16e9
eleanorjw <eleanorjw@users.noreply.github.com>
2021-02-08 06:37:26 +0000

    automated commit by check50 [check50=True]

--- cash.py
+++ cash.py
@@ -3,30 +3,31 @@ from cs50 import get_float
 # Define num of coin
 n = 0
 
-# Get +ve change from user
+# Get +ve change from user and convert to cents
 while True:
-    change = get_float("Change owed: ")
+    change = int(get_float("Change owed: ") * 100)
     if change > 0:
         break
 
+
 # Change of $0.25
-while change >= 0.25:
-    change -= 0.25
+while change >= 25:
+    change -= 25
     n += 1
 
 # Change of $0.10
-while change >= 0.1:
-    change -= 0.1
+while change >= 10:
+    change -= 10
     n += 1
 
 # Change of $0.05    
-while change >= 0.05:
-    change -= 0.05
+while change >= 5:
+    change -= 5
     n += 1
 
 # Change of $0.01   
-while change >= 0.01:
-    change -= 0.01
+while change >= 1:
+    change -= 1
     n += 1
 
 # Print total coins
